9c519ecf0a44ac5db4e9811ed87b64de4f885c3d
Luke Bridges <LukeBridges@users.noreply.github.com>
2018-02-13 08:45:35 +0000

    Synced to latest
    Bug fixes

--- APP.cs
+++ APP.cs
@@ -3,20 +3,20 @@ using Windows.ApplicationModel;
 
 namespace OPG.Signage.Info
 {
-    public static class APP
+	public static class APP
 	{
-        private static string _AppVersion = string.Empty;
+		private static string _AppVersion = string.Empty;
 
-        public static string AppVersion
-        {
-            get
-            {
+		public static string AppVersion
+		{
+			get
+			{
 				if(string.IsNullOrEmpty(_AppVersion))
-                {
-                    _AppVersion = Package.Current.Id.Version.Major.ToString(CultureInfo.CurrentCulture) + "." + Package.Current.Id.Version.Minor.ToString(CultureInfo.CurrentCulture) + "." + Package.Current.Id.Version.Build.ToString(CultureInfo.CurrentCulture);
-                }
-                return _AppVersion;
-            }
-        }
+				{
+					_AppVersion = Package.Current.Id.Version.Major.ToString(CultureInfo.CurrentCulture) + "." + Package.Current.Id.Version.Minor.ToString(CultureInfo.CurrentCulture) + "." + Package.Current.Id.Version.Build.ToString(CultureInfo.CurrentCulture);
+				}
+				return _AppVersion;
+			}
+		}
 	}
 }
--- Network.cs
+++ Network.cs
@@ -5,40 +5,40 @@ using Windows.Networking.Connectivity;
 
 namespace OPG.Signage.Info
 {
-    public static class Network
+	public static class Network
 	{
-        private static string _HostName = string.Empty;
-        private static string _IP = string.Empty;
-        
-        public static string HostName
-        {
-            get
-            {
-                if(String.IsNullOrEmpty(_HostName))
-                {
-                    _HostName = NetworkInformation.GetHostNames().FirstOrDefault(name => name.Type == HostNameType.DomainName)?.DisplayName ?? "???";
-                }
-                return _HostName;
-            }
-        }
+		private static string _HostName = string.Empty;
+		private static string _IP = string.Empty;
+		
+		public static string HostName
+		{
+			get
+			{
+				if(String.IsNullOrEmpty(_HostName))
+				{
+					_HostName = NetworkInformation.GetHostNames().FirstOrDefault(name => name.Type == HostNameType.DomainName)?.DisplayName ?? "???";
+				}
+				return _HostName;
+			}
+		}
 
 		public static string IP
 		{
-            get
-            {
-                if(String.IsNullOrEmpty(_IP))
-                {
-                    if (NetworkInformation.GetInternetConnectionProfile()?.NetworkAdapter == null)
-                    {
-                        return string.Empty;
-                    }
+			get
+			{
+				if(String.IsNullOrEmpty(_IP))
+				{
+					if (NetworkInformation.GetInternetConnectionProfile()?.NetworkAdapter == null)
+					{
+						return string.Empty;
+					}
 
-                    HostName hostname = NetworkInformation.GetHostNames().SingleOrDefault(hn => hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId == NetworkInformation.GetInternetConnectionProfile().NetworkAdapter.NetworkAdapterId);
+					HostName hostname = NetworkInformation.GetHostNames().SingleOrDefault(hn => hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId == NetworkInformation.GetInternetConnectionProfile().NetworkAdapter.NetworkAdapterId);
 
-                    _IP = hostname?.CanonicalName;
-                }
-                return _IP;
-            }
+					_IP = hostname?.CanonicalName;
+				}
+				return _IP;
+			}
 			
 		}
 
